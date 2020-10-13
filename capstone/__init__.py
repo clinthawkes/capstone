1cf4396ca3043a5e75cbe2d79d702fa8f0ef0a54
Susan <hibbert.susan@gmail.com>
2020-10-13 16:20:52 -0700

    Added routes, corrected spelling error

--- capstone/__init__.py
+++ capstone/__init__.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def register():
+    return render_template('login.html')
+
+@app.route('/account')
+def register():
+    return render_template('acount.html')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
