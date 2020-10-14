eeae2cc9e050d8983c6c0cbf86e460c0dd476625
Susan <hibbert.susan@gmail.com>
2020-10-13 20:49:30 -0700

    Prelim pages for website
    Want to add Tooltips for password reqs, add some graphics so it looks more like a banking website

--- capstone/__init__.py
+++ capstone/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template, url_for
 from capstone.db_connector import connect_to_database, execute_query
 
 app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -14,6 +15,10 @@ def register():
 @app.route('/login')
 def login():
     return render_template('login.html')
+    
+@app.route('/login_error')
+def login_error():
+    return render_template('login_error.html')    
 
 @app.route('/account')
 def account():
