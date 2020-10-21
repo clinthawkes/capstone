c844455ad037d021700342fda3112fb77df06d60
Susan <hibbert.susan@gmail.com>
2020-10-21 11:37:51 -0700

    Update index.js

--- capstone/static/script/index.js
+++ capstone/static/script/index.js
@@ -27,12 +27,3 @@ window.addEventListener("load",function() {
         }
     });
 });
-
-document.getElementById("sql1").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "gatesb'-- ";
-});
-
-
-document.getElementById("sql2").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "gatesb' or 1=1 UNION SELECT user, id, balance, password FROM accounts;#";
-});
