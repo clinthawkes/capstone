8973ff0dcdd72c82c79b2969796d46f452d1f6d8
Susan <hibbert.susan@gmail.com>
2020-11-04 12:01:43 -0800

    Security Misconfig- default login and admin page

---
+++ capstone/static/script/login_misconfig.js
@@ -0,0 +1,9 @@
+document.getElementById("misconfig1").addEventListener("click", function(){
+	document.getElementById('UsernameInput').value = "admin";
+	document.getElementById('PasswordInput').value = "admin";
+});
+
+
+document.getElementById("misconfig2").addEventListener("click", function(){
+	document.getElementById('UsernameInput').value = "fakeUsername";
+});
