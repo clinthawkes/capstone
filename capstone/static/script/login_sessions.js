1430e60cbf5decc8591d797365d15807503d52ad
Susan <hibbert.susan@gmail.com>
2020-11-10 12:58:08 -0800

    Implementing Sessions
    Added login_sessions, account_sessions, updated logout route, tidied up init.py (removed unused code, updated some comments)

---
+++ capstone/static/script/login_sessions.js
@@ -0,0 +1,8 @@
+document.getElementById("session1").addEventListener("click", function(){
+	document.getElementById('UsernameInput').value = "to complete";
+});
+
+
+document.getElementById("session2").addEventListener("click", function(){
+	document.getElementById('UsernameInput').value = "to complete";
+});
